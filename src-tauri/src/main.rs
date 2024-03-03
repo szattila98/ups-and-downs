9115154e35b638f9d1cc543f2abb9574540d7261
Attila Szőke <szokeattila9801@gmail.com>
2024-03-03 15:05:43 +0100

    feat: commands are now typesafe thanks to specta

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,13 +3,24 @@
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
+#[specta::specta]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
 fn main() {
+    let specta_builder = {
+        let specta_builder =
+            tauri_specta::ts::builder().commands(tauri_specta::collect_commands![greet]);
+
+        #[cfg(debug_assertions)]
+        let specta_builder = specta_builder.path("../src/bindings.ts");
+
+        specta_builder.into_plugin()
+    };
+
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .plugin(specta_builder)
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/lib/Greet.svelte
+++ src/lib/Greet.svelte
@@ -1,12 +1,12 @@
 <script lang="ts">
-	import { invoke } from '@tauri-apps/api/tauri';
+	import { commands } from '../bindings';
 
 	let name = '';
 	let greetMsg = '';
 
 	async function greet() {
 		// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-		greetMsg = await invoke('greet', { name });
+		greetMsg = await commands.greet(name);
 	}
 </script>
 
