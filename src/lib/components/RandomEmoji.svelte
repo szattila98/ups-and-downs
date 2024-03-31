ef49417d3e9e793053a56bdcf80d94e938419d67
Attila Szőke <szokeattila9801@gmail.com>
2024-03-31 11:49:25 +0200

    fix: random emojis are fixed in dimensions

--- src/lib/components/RandomEmoji.svelte
+++ src/lib/components/RandomEmoji.svelte
@@ -6,14 +6,24 @@
 	$: emoji = emojis[Math.floor(Math.random() * emojis.length)];
 </script>
 
-{#if emoji.startsWith('&#')}
-	<!-- eslint-disable svelte/no-at-html-tags -->
-	<span class="text-emoji">{@html emoji}</span>
-{:else}
-	<img class="img-emoji" src={`/img/emojis/${emoji}`} alt={emoji.split('.')[0]} />
-{/if}
+<div class="emoji-container">
+	{#if emoji.startsWith('&#')}
+		<!-- eslint-disable svelte/no-at-html-tags -->
+		<span class="text-emoji">{@html emoji}</span>
+	{:else}
+		<img class="img-emoji" src={`/img/emojis/${emoji}`} alt={emoji.split('.')[0]} />
+	{/if}
+</div>
 
 <style scoped>
+	.emoji-container {
+		align-items: center;
+		display: flex;
+		height: var(--emoji-container-height, 32px);
+		justify-content: center;
+		width: var(--emoji-container-width, 32px);
+	}
+
 	.text-emoji {
 		font-size: var(--emoji-font-size, 24px);
 	}
